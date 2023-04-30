feb976fd474aba8c8c731333c6b565a2d67463c9
CChung <curtischung06@gmail.com>
2023-04-30 01:16:57 -0400

    Updated charts to reflect price movement ratios and added upper and lower boundaries to trigger alert when breached

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,38 @@
 import { ServerRespond } from './DataStreamer';
 
+// Define the value type the schema must have
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+    price_abc: number,
+    price_def: number,
+    ratio: number,
+    timestamp: Date,
+    upper_bound: number,
+    lower_bound: number,
+    trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
-  }
+    static generateRow(serverResponds: ServerRespond[]) {
+        // Create boundaries to trigger alert when there is "noticeable" price movements
+        const upperBound = 1 + 0.03;
+        const lowerBound = 1 - 0.03;
+
+        // Get ratio of avg price between ABC and DEF
+        const stockABC = serverResponds[0];
+        const stockDEF = serverResponds[1];
+        const avgPriceABC = (stockABC.top_ask.price + stockABC.top_bid.price) / 2;
+        const avgPriceDEF = (stockDEF.top_ask.price + stockDEF.top_bid.price) / 2;
+        const avgPriceRatio = avgPriceABC / avgPriceDEF;
+
+        return {
+            price_abc: avgPriceABC,
+            price_def: avgPriceDEF,
+            ratio: avgPriceRatio,
+            timestamp: stockABC.timestamp > stockDEF.timestamp ? stockABC.timestamp : stockDEF.timestamp,
+            upper_bound: upperBound,
+            lower_bound: lowerBound,
+            trigger_alert: (avgPriceRatio > upperBound || avgPriceRatio < lowerBound) ? avgPriceRatio : undefined,
+        };
+    }
 }
